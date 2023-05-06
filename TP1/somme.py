e7b023a57086174073713626e83a6d6e3fdae806
petitlaye03 <ndourabdoulaye1803@gmail.com>
2023-05-06 11:09:39 +0000

    First code added

---
+++ TP1/somme.py
@@ -0,0 +1,9 @@
+# trouver la somme de deux nombres entiers
+nbr1 = int(input("Entrez le premier nombre: "))
+nbr2 = int(input("Entrez le second nombre: "))
+
+# Additionner les deux nombres
+s = nbr1 + nbr2
+
+# Afficher la somme
+print("La somme de {0} et {1} est {2}".format(nbr1, nbr2, s))
